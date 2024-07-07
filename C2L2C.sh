613f10dbc5410dc89113a3dbbae4486371be9af5
Alex Mauzon <mauzon@gmail.com>
2024-07-07 11:31:05 +0300

    Update C2L2C.sh
    Fix for encoding issue

--- C2L2C.sh
+++ C2L2C.sh
@@ -14,6 +14,8 @@
 # @raycast.author Alex Mauzon
 # @raycast.authorURL https://github.com/mauzon
 
+export LANG="en_US.UTF-8"
+
 input=$1
 
 eng_chars=("q" "w" "e" "r" "t" "y" "u" "i" "o" "p" "[" "]" "a" "s" "d" "f" "g" "h" "j" "k" "l" ";" "'" "z" "x" "c" "v" "b" "n" "m" "," "." "/" "Q" "W" "E" "R" "T" "Y" "U" "I" "O" "P" "{" "}" "A" "S" "D" "F" "G" "H" "J" "K" "L" ":" "\"" "Z" "X" "C" "V" "B" "N" "M" "<" ">" "?")
